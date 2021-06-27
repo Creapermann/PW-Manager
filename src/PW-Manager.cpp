356dc5ae86921db3bdabe0fbe71e22b64d9178bb
Ahmed Mostafa <amk67482@gmail.com>
2021-06-27 17:53:21 +0200

    Add project files.

---
+++ src/PW-Manager.cpp
@@ -0,0 +1,12 @@
+﻿// PW-Manager.cpp : Defines the entry point for the application.
+//
+
+#include "PW-Manager.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
