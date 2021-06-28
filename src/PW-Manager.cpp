3efad238060d19c6c33b2163ffbd2e9d321f3e33
Creapermann <creapermann37@gmail.com>
2021-06-28 09:59:29 +0200

    Small refactoring

--- src/PW-Manager.cpp
+++ src/PW-Manager.cpp
@@ -2,49 +2,67 @@
 #include "ProgramInfo.h"
 
 
+void test()
+{
+	std::cout << "a\n";
+}
+
+
 int main(void)
 {
+	// The screen
 	auto screen = ftxui::ScreenInteractive::Fullscreen();
 
-	// Define the menu
+
+#pragma region Components
+
+	/*Menu*/
+	// Menu items
 	std::vector<std::wstring> entries = {
 		L"Login",
 		L"Create New Account",
 		L"Exit",
 	};
-
+	// Selected item
 	int selected = 0;
+	// Menu
 	auto menu = ftxui::Menu(&entries, &selected);
-
-	// ftxui::MenuBase::From(menu)->selected_style = ftxui::bold;
+	// Addaptions
+	ftxui::MenuBase::From(menu)->on_enter = screen.ExitLoopClosure();
 	ftxui::MenuBase::From(menu)->focused_style = ftxui::bgcolor(ftxui::Color::Blue);
 
-	ftxui::MenuBase::From(menu)->on_enter = screen.ExitLoopClosure();
+#pragma endregion
+
+
+	
+	
+
 
 	int x = 0;
 	ftxui::Component container = ftxui::Container::Vertical({ menu }, &x);
-
-	auto renderer = ftxui::Renderer(container, [&] 
+	auto renderer = ftxui::Renderer(container, [&]
 	{
 			return
 				ftxui::vbox
-				({
+				(
 					ftxui::hbox
-					({
-						//Title
+					(
+						// Title
 						ftxui::text(std::wstring(pInfo::name.begin(), pInfo::name.end())) | ftxui::flex | ftxui::bold | ftxui::color(ftxui::Color::Green),
-						ftxui::text(std::wstring(pInfo::version.begin(), pInfo::version.end())),
-						// Separator
-					}) | ftxui::border,
 
-					//Seperator
+						// Version
+						ftxui::text(L"v_" + std::wstring(pInfo::version.begin(), pInfo::version.end()))
+
+					) | ftxui::border,
+
+					// Seperator
 					ftxui::separator(),
 
-					ftxui::vbox({
-						//Menu
+					ftxui::vbox(
+						// Menu
 						menu->Render()
-					}) | ftxui::border,
-			});
+					) | ftxui::border
+				);
 	});
 	screen.Loop(renderer);
 
