0d855be47050b96d6a2f9945f4e2e68830cf2709
Creapermann <creapermann37@gmail.com>
2021-06-29 15:36:22 +0200

    CMakeLists.txt fixed to set c++ standard on c++17

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,9 +4,22 @@
 
 int main()
 {
-	// Starts the startwindow which lets the user login or register
-	//auto screen = ftxui::ScreenInteractive::Fullscreen();
 
+	if (__cplusplus == 201703L) 
+		std::cout << "C++17\n";
+	else if (__cplusplus == 201402L) 
+		std::cout << "C++14\n";
+	else if (__cplusplus == 201103L) 
+		std::cout << "C++11\n";
+	else if (__cplusplus == 199711L) 
+		std::cout << "C++98\n";
+	else
+		std::cout << "pre-standard C++\n";
+
+	// Starts the startwindow which lets the user login or register
+	//auto screen = ftxui::ScreenInteractive::Fullscreen(); 
+	//Do u still wanna try?
+	// ok as you see and wan
 	showStartWindow(ftxui::ScreenInteractive::Fullscreen());
 
 	
