5c63cc250a8a8c06e0410358015fa4d53c576e4c
Ahmed Mostafa <amk67482@gmail.com>
2021-06-30 01:55:48 +0200

    Add SQLite3 library

--- src/Model/FtxuiIncludes.h
+++ src/Model/FtxuiIncludes.h
@@ -3,15 +3,21 @@
 
 #pragma once
 
+// Standard Library
 #include <iostream>
 #include <vector>
 #include <string>
 
+// FTXUI Library
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/input.hpp>
 #include <ftxui/component/menu.hpp>
 #include <ftxui/component/screen_interactive.hpp>
+
+// SQLite3 Library
+#include "../lib/sqlite/sqlite3.h"
+
 
 // TODO: Reference additional headers your program requires here.
