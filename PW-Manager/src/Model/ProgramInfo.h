306433c2e58ca4d539cf025d1c365745da224082
Creapermann <creapermann37@gmail.com>
2021-08-23 21:45:19 +0200

    Version 0.9.0

--- PW-Manager/src/Model/ProgramInfo.h
+++ PW-Manager/src/Model/ProgramInfo.h
@@ -4,7 +4,7 @@
 
 namespace pInfo
 {
-	const std::string version = "0.7.1";
+	const std::string version = "0.9.0";
 	const std::string name = "PW-Manager";
 	const std::string website = "https://PW-Manager.com";
 }
